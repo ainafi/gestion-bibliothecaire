f96e8fc731171f2baf2f603dfae7db219ad9ca7e
ainafi <razanaminoainafinaritra@gmail.com>
2024-09-17 15:30:29 +0300

    modif prete done

--- modifierunpret.cpp
+++ modifierunpret.cpp
@@ -1,5 +1,11 @@
 #include "modifierunpret.h"
 #include "ui_modifierunpret.h"
+#include <QSqlDatabase>
+#include <QSqlQuery>
+#include <QSqlError>
+#include <QDebug>
+#include <QMessageBox>
+#include <QDate>
 
 modifierUnPret::modifierUnPret(QWidget *parent)
     : QDialog(parent)
@@ -11,4 +17,97 @@ modifierUnPret::modifierUnPret(QWidget *parent)
 modifierUnPret::~modifierUnPret()
 {
     delete ui;
+}
+
+void modifierUnPret::setPretInfo(const QString &numPret, const QString &numLecteur, const QString &numLivre, const QDate &datePret, const QDate &dateRetour)
+{
+    ui->numeroDuPrete->setText(numPret);
+    ui->numeroLecteur->setText(numLecteur);
+    ui->numeroLivre->setText(numLivre);
+    ui->datePret->setDate(datePret);
+    ui->dateRetour->setDate(dateRetour);
+}
+
+void modifierUnPret::on_Recherche_clicked()
+{
+    QString numPret = ui->numeroDuPrete->text();
+
+    QSqlDatabase database;
+    if (QSqlDatabase::contains("qt_sql_default_connection")) {
+        database = QSqlDatabase::database("qt_sql_default_connection");
+    } else {
+        database = QSqlDatabase::addDatabase("QMYSQL");
+        database.setHostName("localhost");
+        database.setDatabaseName("GestionBibliotecaire");
+        database.setUserName("gestionuser");
+        database.setPassword("password");
+    }
+
+    if (!database.open()) {
+        qDebug() << "Error: connection with database failed - " << database.lastError().text();
+        QMessageBox::critical(this, "Database Connection Error", database.lastError().text());
+        return;
+    }
+
+    QSqlQuery query;
+    query.prepare("SELECT numLecteur, numLivre, datePret, dateRetour FROM Pret WHERE numPret = :numPret");
+    query.bindValue(":numPret", numPret);
+    if (!query.exec()) {
+        qDebug() << "Select error:" << query.lastError().text();
+        QMessageBox::critical(this, "Error", "Failed to retrieve loan data");
+        return;
+    }
+
+    if (query.next()) {
+        QString numLecteur = query.value("numLecteur").toString();
+        QString numLivre = query.value("numLivre").toString();
+        QDate datePret = query.value("datePret").toDate();
+        QDate dateRetour = query.value("dateRetour").toDate();
+
+        setPretInfo(numPret, numLecteur, numLivre, datePret, dateRetour);
+    } else {
+        QMessageBox::warning(this, "Error", "Loan number not found");
+    }
+}
+
+void modifierUnPret::on_Modifier_clicked()
+{
+    QString numPret = ui->numeroDuPrete->text();
+    QString numLecteur = ui->numeroLecteur->text();
+    QString numLivre = ui->numeroLivre->text();
+    QDate datePret = ui->datePret->date();
+    QDate dateRetour = ui->dateRetour->date();
+
+    QSqlDatabase database;
+    if (QSqlDatabase::contains("qt_sql_default_connection")) {
+        database = QSqlDatabase::database("qt_sql_default_connection");
+    } else {
+        database = QSqlDatabase::addDatabase("QMYSQL");
+        database.setHostName("localhost");
+        database.setDatabaseName("GestionBibliotecaire");
+        database.setUserName("gestionuser");
+        database.setPassword("password");
+    }
+
+    if (!database.open()) {
+        qDebug() << "Error: connection with database failed - " << database.lastError().text();
+        QMessageBox::critical(this, "Database Connection Error", database.lastError().text());
+        return;
+    }
+
+    QSqlQuery query;
+    query.prepare("UPDATE Pret SET numLecteur = :numLecteur, numLivre = :numLivre, datePret = :datePret, dateRetour = :dateRetour WHERE numPret = :numPret");
+    query.bindValue(":numPret", numPret);
+    query.bindValue(":numLecteur", numLecteur);
+    query.bindValue(":numLivre", numLivre);
+    query.bindValue(":datePret", datePret);
+    query.bindValue(":dateRetour", dateRetour);
+
+    if (!query.exec()) {
+        qDebug() << "Update error:" << query.lastError().text();
+        QMessageBox::critical(this, "Error", "Failed to update loan data");
+        return;
+    }
+
+    QMessageBox::information(this, "Success", "Loan data updated successfully");
 }
--- modifierunpret.h
+++ modifierunpret.h
@@ -15,6 +15,12 @@ public:
     explicit modifierUnPret(QWidget *parent = nullptr);
     ~modifierUnPret();
 
+    void setPretInfo(const QString &numPret, const QString &numLecteur, const QString &numLivre, const QDate &datePret, const QDate &dateRetour);
+
+private slots:
+    void on_Recherche_clicked();
+    void on_Modifier_clicked(); // Slot for the update button
+
 private:
     Ui::modifierUnPret *ui;
 };
