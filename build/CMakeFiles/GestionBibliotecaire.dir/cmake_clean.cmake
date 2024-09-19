8e601fbf87b73e78435e281d488654719cf9b0f2
ainafi <razanaminoainafinaritra@gmail.com>
2024-09-19 14:26:25 +0300

    every thing is done

---
+++ build/CMakeFiles/GestionBibliotecaire.dir/cmake_clean.cmake
@@ -0,0 +1,42 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/GestionBibliotecaire_autogen.dir/AutogenUsed.txt"
+  "CMakeFiles/GestionBibliotecaire_autogen.dir/ParseCache.txt"
+  "GestionBibliotecaire_autogen"
+  "CMakeFiles/GestionBibliotecaire.dir/GestionBibliotecaire_autogen/mocs_compilation.cpp.o"
+  "CMakeFiles/GestionBibliotecaire.dir/GestionBibliotecaire_autogen/mocs_compilation.cpp.o.d"
+  "CMakeFiles/GestionBibliotecaire.dir/ajouterunlivre.cpp.o"
+  "CMakeFiles/GestionBibliotecaire.dir/ajouterunlivre.cpp.o.d"
+  "CMakeFiles/GestionBibliotecaire.dir/createlecteur.cpp.o"
+  "CMakeFiles/GestionBibliotecaire.dir/createlecteur.cpp.o.d"
+  "CMakeFiles/GestionBibliotecaire.dir/listlecteur.cpp.o"
+  "CMakeFiles/GestionBibliotecaire.dir/listlecteur.cpp.o.d"
+  "CMakeFiles/GestionBibliotecaire.dir/listpret.cpp.o"
+  "CMakeFiles/GestionBibliotecaire.dir/listpret.cpp.o.d"
+  "CMakeFiles/GestionBibliotecaire.dir/main.cpp.o"
+  "CMakeFiles/GestionBibliotecaire.dir/main.cpp.o.d"
+  "CMakeFiles/GestionBibliotecaire.dir/mainwindow.cpp.o"
+  "CMakeFiles/GestionBibliotecaire.dir/mainwindow.cpp.o.d"
+  "CMakeFiles/GestionBibliotecaire.dir/modifierunlecteur.cpp.o"
+  "CMakeFiles/GestionBibliotecaire.dir/modifierunlecteur.cpp.o.d"
+  "CMakeFiles/GestionBibliotecaire.dir/modifierunlivre.cpp.o"
+  "CMakeFiles/GestionBibliotecaire.dir/modifierunlivre.cpp.o.d"
+  "CMakeFiles/GestionBibliotecaire.dir/modifierunpret.cpp.o"
+  "CMakeFiles/GestionBibliotecaire.dir/modifierunpret.cpp.o.d"
+  "CMakeFiles/GestionBibliotecaire.dir/prete.cpp.o"
+  "CMakeFiles/GestionBibliotecaire.dir/prete.cpp.o.d"
+  "CMakeFiles/GestionBibliotecaire.dir/supprimerlecteur.cpp.o"
+  "CMakeFiles/GestionBibliotecaire.dir/supprimerlecteur.cpp.o.d"
+  "CMakeFiles/GestionBibliotecaire.dir/supprimerunlivre.cpp.o"
+  "CMakeFiles/GestionBibliotecaire.dir/supprimerunlivre.cpp.o.d"
+  "CMakeFiles/GestionBibliotecaire.dir/supprimerunpret.cpp.o"
+  "CMakeFiles/GestionBibliotecaire.dir/supprimerunpret.cpp.o.d"
+  "GestionBibliotecaire"
+  "GestionBibliotecaire.pdb"
+  "GestionBibliotecaire_autogen/mocs_compilation.cpp"
+  "GestionBibliotecaire_autogen/timestamp"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/GestionBibliotecaire.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
